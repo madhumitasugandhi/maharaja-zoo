288f87251c743a6ff05475e53cc14aca46f04685
madhumitasugandhi <madhumitasugandhi@gmail.com>
2025-10-13 11:26:34 +0530

    Home page code updated.

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -420,15 +420,16 @@
                 <tr>
                   <td>Ticket</td>
                   <td>
-                    <p class="mb-2">9423636143</p>
-                    <p class="mb-0">ticket@example.com</p>
+                    <p class="mb-2">Adult- 40</p>
+                    <p class="mb-2">Children – 20</p>
+                    <p class="mb-2">Children park - 5</p>
                   </td>
                 </tr>
                 <tr>
                   <td>Support</td>
                   <td>
-                    <p class="mb-2">+012 345 6789</p>
-                    <p class="mb-0">support@example.com</p>
+                    <p class="mb-2">9423636143</p>
+                    <p class="mb-0">info@gmail.com</p>
                   </td>
                 </tr>
               </tbody>
@@ -438,81 +439,6 @@
       </div>
     </div>
     <!-- Visiting Hours End -->
-
-    <!-- Membership Start -->
-    <div class="container-xxl py-5">
-      <div class="container">
-        <div
-          class="row g-5 mb-5 align-items-end wow fadeInUp"
-          data-wow-delay="0.1s"
-        >
-          <div class="col-lg-6">
-            <p><span class="text-primary me-2">#</span>Membership</p>
-            <h1 class="display-5 mb-0">
-              You Can Be A Proud Member Of
-              <span class="text-primary">Maharaj Bagh</span>
-            </h1>
-          </div>
-          <div class="col-lg-6 text-lg-end">
-            <a class="btn btn-primary py-3 px-5" href="">Special Pricing</a>
-          </div>
-        </div>
-        <div class="row g-4">
-          <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
-            <div class="membership-item position-relative">
-              <img class="img-fluid" src="img/animal-lg-1.jpg" alt="" />
-              <h1 class="display-1">01</h1>
-              <h4 class="text-white mb-3">Popular</h4>
-              <h3 class="text-primary mb-4">$99.00</h3>
-              <p><i class="fa fa-check text-primary me-3"></i>10% discount</p>
-              <p>
-                <i class="fa fa-check text-primary me-3"></i>2 adult and 2 child
-              </p>
-              <p>
-                <i class="fa fa-check text-primary me-3"></i>Free animal
-                exhibition
-              </p>
-              <a class="btn btn-outline-light px-4 mt-3" href="">Get Started</a>
-            </div>
-          </div>
-          <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
-            <div class="membership-item position-relative">
-              <img class="img-fluid" src="img/animal-lg-2.jpg" alt="" />
-              <h1 class="display-1">02</h1>
-              <h4 class="text-white mb-3">Standard</h4>
-              <h3 class="text-primary mb-4">$149.00</h3>
-              <p><i class="fa fa-check text-primary me-3"></i>15% discount</p>
-              <p>
-                <i class="fa fa-check text-primary me-3"></i>4 adult and 4 child
-              </p>
-              <p>
-                <i class="fa fa-check text-primary me-3"></i>Free animal
-                exhibition
-              </p>
-              <a class="btn btn-outline-light px-4 mt-3" href="">Get Started</a>
-            </div>
-          </div>
-          <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
-            <div class="membership-item position-relative">
-              <img class="img-fluid" src="img/animal-lg-3.jpg" alt="" />
-              <h1 class="display-1">03</h1>
-              <h4 class="text-white mb-3">Premium</h4>
-              <h3 class="text-primary mb-4">$199.00</h3>
-              <p><i class="fa fa-check text-primary me-3"></i>20% discount</p>
-              <p>
-                <i class="fa fa-check text-primary me-3"></i>6 adult and 6 child
-              </p>
-              <p>
-                <i class="fa fa-check text-primary me-3"></i>Free animal
-                exhibition
-              </p>
-              <a class="btn btn-outline-light px-4 mt-3" href="">Get Started</a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- Membership End -->
 
     <!-- Testimonial Start -->
     <div class="container-xxl py-5">
